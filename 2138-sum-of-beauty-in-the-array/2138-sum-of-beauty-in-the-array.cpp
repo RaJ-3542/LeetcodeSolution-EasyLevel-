154f927e3a3d57a518ea915f802a5e924f3c761c
Raj Aryan <91794070+RaJ-3542@users.noreply.github.com>
2023-10-25 23:35:57 +0530

    Time: 328 ms (8.72%), Space: 93.5 MB (7.99%) - LeetHub

---
+++ 2138-sum-of-beauty-in-the-array/2138-sum-of-beauty-in-the-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        
+        int maxx = INT_MIN;
+        int minn = INT_MAX;
+        vector<int> dp;
+        vector<int> dp2(nums.size(),0);
+        for(int i = 0; i<nums.size();i++){
+            maxx = max(maxx,nums[i]);
+            dp.push_back(maxx);
+        }
+        for(int i = nums.size()-1;i>=0;i--){
+            minn = min(minn,nums[i]);
+            dp2[i] = minn;
+
+        }
+        
+        int sum=0;
+        for(int i = 1; i<=nums.size()-2;i++){
+            if(dp[i-1]<nums[i] && nums[i]<dp2[i+1]){
+                sum+=2;
+            }
+            else if(nums[i]>nums[i-1] && nums[i]<nums[i+1]){
+                sum+=1;
+            }
+            else{
+                sum+=0;
+            }
+            
+        }
+        for(auto a : nums){
+            cout<<a<<" ";
+        }
+        return sum;
+
+        // return accumulate(nums.begin()+1,nums.end()-1,0);
+        
+    }
+};
